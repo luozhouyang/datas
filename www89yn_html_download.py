ff4c72bf9b3b600dfb23a9bb65d87dab17344481
luozhouyang <stupidme.me.lzy@gmail.com>
2018-05-16 22:30:23 +0800

    Download html page

---
+++ www89yn_html_download.py
@@ -0,0 +1,17 @@
+import time
+from urllib import request
+
+for id in range(700064, 800000):
+    try:
+        time.sleep(1)
+        resp = request.urlopen('http://www.89yn.com/member.asp?id=' + str(id), timeout=2.0)
+        if resp.getcode() != 200:
+            continue
+        page = resp.read().decode('gbk')
+        file = '/home/allen/PycharmProjects/datas/data/' + str(id) + '.txt'
+        with open(file, mode='wt', encoding='utf-8', buffering=8192) as f:
+            f.write(page)
+    except Exception as e:
+        continue
+    else:
+        print(str(id))
---
+++ zuyouw.py
@@ -0,0 +1,32 @@
+import time
+from urllib import request
+import chardet
+
+headers = {
+    'Connection': 'keep-alive',
+    'Cache-Control': 'max-age=0',
+    'Accept': 'text/html, */*; q=0.01',
+    'X-Requested-With': 'XMLHttpRequest',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.89 Safari/537.36',
+    'DNT': '1',
+    'Accept-Encoding': 'gzip, deflate, sdch',
+    'Accept-Language': 'zh-CN,zh;q=0.8,ja;q=0.6'
+}
+for id in range(162315, 167900):
+    try:
+        time.sleep(1)
+        url = 'http://www.zuyouw.com/home/' + str(id)
+        print(url)
+        req = request.Request(url=url, headers=headers)
+        resp = request.urlopen(req, timeout=2.0)
+        if resp.getcode() != 200:
+            continue
+        page = resp.read().decode('gb2312')
+        file = '/home/allen/PycharmProjects/datas/zuyouw_data/' + str(id) + '.txt'
+        with open(file, mode='wt', encoding='utf-8', buffering=8192) as f:
+            f.write(page)
+    except Exception as e:
+        print(e)
+        continue
+    else:
+        print(str(id))

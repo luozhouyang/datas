07b6e7d8b89350d675fdd3435960ae179e8f0b46
luozhouyang <stupidme.me.lzy@gmail.com>
2018-05-23 22:50:00 +0800

    Fixed wrong key order

--- www89yn/www89yn_csv_visualizer.py
+++ www89yn/www89yn_csv_visualizer.py
@@ -1,5 +1,5 @@
 import csv
-from collections import Counter
+from collections import Counter, OrderedDict
 
 import matplotlib.pyplot as plt
 
@@ -30,7 +30,7 @@ class Visualizer:
         origins_women = []
 
         def callback(row):
-            age = row[3].replace('岁', '')
+            age = str(row[3].replace('岁', ''))
             gender = row[2].strip()
             education = row[9].strip()
             origin = row[13].strip()
@@ -49,13 +49,13 @@ class Visualizer:
 
         self._read_csv_file(callback)
 
-        self.ages_dict = Counter(ages)
-        self.ages_men_dict = Counter(ages_men)
-        self.ages_women_dict = Counter(ages_women)
-        self.genders_dict = Counter(genders)
-        self.educations_dict = Counter(educations)
-        self.educations_men_dict = Counter(educations_men)
-        self.educations_women_dict = Counter(educations_women)
+        self.ages_dict = OrderedDict(sorted(Counter(ages).items()))
+        self.ages_men_dict = OrderedDict(sorted(Counter(ages_men).items()))
+        self.ages_women_dict = OrderedDict(sorted(Counter(ages_women).items()))
+        self.genders_dict = OrderedDict(sorted(Counter(genders).items()))
+        self.educations_dict = OrderedDict(sorted(Counter(educations).items()))
+        self.educations_men_dict = OrderedDict(sorted(Counter(educations_men).items()))
+        self.educations_women_dict = OrderedDict(sorted(Counter(educations_women).items()))
 
         self.has_parse_file = True
 
@@ -66,43 +66,35 @@ class Visualizer:
             for row in reader:
                 parse_line_callback(row)
 
-    def _age_dict(self):
-        age_dict = {}
-        for i in range(15, 50):
-            age_dict[i] = 0
-
-        def parse_age(row):
-            age_dict[int(row[3].replace('岁', ''))] += 1
-
-        self._read_csv_file(parse_age)
-        return age_dict
-
     def visualize_age_line(self):
-        age_dict = self._age_dict()
-        plt.figure()
+        if not self.has_parse_file:
+            self.parse_csv_file()
+        plt.figure(figsize=(10, 7))
         plt.title('Age distribution line chart')
-        plt.plot(age_dict.keys(), age_dict.values())
+        plt.plot(self.ages_dict.keys(), self.ages_dict.values())
         plt.xlabel('Age')
         plt.savefig("images/age_line.png")
 
     def visualize_age_pie(self):
-        age_dict = self._age_dict()
+        if not self.has_parse_file:
+            self.parse_csv_file()
+        # age_dict = self._age_dict()
         total = 0
-        for _, v in age_dict.items():
+        for _, v in self.ages_dict.items():
             total += v
-        plt.figure()
+        plt.figure(figsize=(8, 8))
         plt.title('Age distribution pie chart')
         labels = []
-        for k in age_dict.keys():
-            v = "%s - %.2f" % (k, (age_dict[k] / total * 100))
+        for k in self.ages_dict.keys():
+            v = "%s - %.2f" % (k, (int(self.ages_dict[k]) / total * 100))
             labels.append(v + "%")
-        plt.pie(age_dict.values(), labels=labels)
+        plt.pie(self.ages_dict.values(), labels=labels)
         plt.savefig('images/age_pie.png')
 
     def visualize_age_men_line(self):
         if not self.has_parse_file:
             self.parse_csv_file()
-        plt.figure(figsize=(7, 7))
+        plt.figure(figsize=(8, 8))
         plt.title("Male age distribution", )
         # total = 0
         # for v in self.ages_men_dict.values():
@@ -110,7 +102,7 @@ class Visualizer:
         # labels = []
         # for k in self.ages_men_dict.keys():
         #     labels.append("%s - %.2f" % (k, int(k) / total))
-        plt.pie(sorted(self.ages_men_dict.values()), labels=sorted(self.ages_men_dict.keys()))
+        plt.pie(self.ages_men_dict.values(), labels=self.ages_men_dict.keys())
         # plt.legend()
         plt.savefig('images/age_men_pie.png')
 
@@ -118,6 +110,6 @@ class Visualizer:
 if __name__ == "__main__":
     v = Visualizer("/home/allen/PycharmProjects/datas/www89yn_data/info.csv")
     v.parse_csv_file()
-    # v.visualize_age_line()
-    # v.visualize_age_pie()
+    v.visualize_age_line()
+    v.visualize_age_pie()
     v.visualize_age_men_line()
